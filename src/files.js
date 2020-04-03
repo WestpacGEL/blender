28998d1b40cc1186ac516f4f2efb935bb748309d
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-04-03 13:31:16 +1100

    added comments, debug logs, createDir
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/files.js
+++ src/files.js
@@ -1,18 +1,25 @@
 /**
- * The files store
+ * All functions to store and save files
  *
- * @type {Object}
- */
-
+ * FILES     - The files store
+ * saveFiles - Save Files as zip or individually to disk or return
+ * writeFile - Write a file to a directory
+ * createDir - Create all directories within a given path
+ **/
+const archiver = require('archiver');
 const path = require('path');
 const fs = require('fs');
-const archiver = require('archiver');
 
 const { SETTINGS } = require('./settings.js');
 const { LOADING } = require('./loading.js');
 const { DEBUG, D } = require('./log.js');
 const { color } = require('./color.js');
 
+/**
+ * The files store
+ *
+ * @type {Object}
+ */
 const FILES = {
 	store: new Map(),
 
@@ -34,17 +41,15 @@ const FILES = {
 };
 
 /**
- * Save Files function
+ * Save Files as zip or individually to disk or return
  *
- * @return {type}  - Return a promise
+ * @return {promise object}  - Return a promise
  */
 function saveFiles() {
 	D.header('saveFiles');
 
 	return new Promise((resolve, reject) => {
 		LOADING.start = { total: FILES.get.size };
-
-		D.log(`Settings: ${JSON.stringify(SETTINGS.get)}`);
 
 		const result = {
 			code: 0,
@@ -59,46 +64,45 @@ function saveFiles() {
 			const archive = archiver('zip');
 
 			// catch warnings
-			archive.on('warning', (err) => {
+			archive.on('warning', (error) => {
 				result.code = 1;
 				if (err.code === 'ENOENT') {
-					result.errors.push(`Could not find file or directory: ${err}`);
+					result.errors.push(`Could not find file or directory: ${error}`);
 				} else {
-					result.errors.push(`Error archiving files: ${err}`);
+					result.errors.push(`Error archiving files: ${error}`);
 				}
 			});
 
 			// catch errors
-			archive.on('error', (err) => {
+			archive.on('error', (error) => {
 				result.code = 1;
-				result.errors.push(`Error archiving files: ${err}`);
+				result.errors.push(`Error archiving files: ${error}`);
 			});
 
 			let zipOutput = null;
 
 			// write to disk if output is provided
 			if (SETTINGS.get.output) {
-				// constract the path we'll be outputing the zip file to
+				D.log(`Writing zip to disk`);
+
+				// construct the path we'll be outputting the zip file to
 				const outputPath = path.resolve(process.cwd(), SETTINGS.get.output);
 
 				// create directory if it doesn't already exist
-				if (!fs.existsSync(outputPath)) {
-					fs.mkdirSync(outputPath, { recursive: true }, (err) => {
-						if (err) {
-							result.code = 1;
-							result.errors.push(`Error creating directory: ${err}`);
-							reject(result);
-						}
-					});
+				const dirCreation = createDir(outputPath);
+				if (dirCreation !== 'success') {
+					result.code = 1;
+					result.errors.push(dirCreation);
+					reject(result);
 				}
 
 				const zipPath = `${outputPath}/blender.zip`;
 				zipOutput = fs.createWriteStream(zipPath);
 
-				zipOutput.on('close', (err) => {
-					if (err) {
+				zipOutput.on('close', (error) => {
+					if (error) {
 						result.code = 1;
-						result.errors.push(`Error archiving files: ${err}`);
+						result.errors.push(`Error archiving files: ${error}`);
 					}
 					D.log(`Zip file written to path: ${color.yellow(zipPath)}`);
 					if (result.code === 1) {
@@ -109,8 +113,9 @@ function saveFiles() {
 				});
 			}
 
+			// append each file to the zip
 			FILES.get.forEach(({ name, path, content }) => {
-				// append each file to the zip
+				D.log(`Appending ${color.yellow(name)} to zip archive`);
 				archive.append(content, { name: `${path}/${name}` }, 'utf-8');
 				LOADING.tick();
 			});
@@ -123,8 +128,10 @@ function saveFiles() {
 			// finish off zip process
 			archive.finalize();
 
-			// if we aren't outputing it anywhere, just return the zip directly
+			// if we aren't outputting it anywhere, just return the zip directly
 			if (!SETTINGS.get.output) {
+				D.log(`Zip not saved but returned`);
+
 				LOADING.abort();
 				result.files = [{ name: 'blender.zip', path: '', content: archive }];
 				if (result.code === 1) {
@@ -134,23 +141,29 @@ function saveFiles() {
 				}
 			}
 		} else if (SETTINGS.get.output) {
-			D.log(`Saving files`);
+			D.log(`Saving files individually`);
 
 			const allFiles = [];
 
-			FILES.get.forEach(async ({ name, path, content, category }) => {
-				// save all files to a directory
-				allFiles.push(writeFile(name, path, content));
+			// save all files to a directory
+			FILES.get.forEach(async ({ name, path, content }) => {
+				D.log(`Saving file ${color.yellow(name)} to ${color.yellow(path)}`);
+				try {
+					allFiles.push(await writeFile(name, path, content));
+				} catch (error) {
+					reject(error);
+				}
 				LOADING.tick();
 			});
 
 			Promise.all(allFiles)
 				.then(() => {
+					D.log(`All ${color.yellow(allFiles.length)} files saved`);
 					LOADING.abort();
 					resolve(result);
 				})
-				.catch((err) => {
-					reject(err);
+				.catch((error) => {
+					reject(error);
 				});
 		} else {
 			D.log(`Returning files directly`);
@@ -163,12 +176,13 @@ function saveFiles() {
 }
 
 /**
- * Write the files
+ * Write a file to a directory
  *
- * @param  {type} fileName    - Name of the file
- * @param  {type} outputPath  - Where the file gets outputted
- * @param  {type} fileContent - Contents of the file
- * @return {type}             - Return a promise
+ * @param  {string} fileName    - Name of the file
+ * @param  {string} outputPath  - Path of the file
+ * @param  {string} fileContent - Contents of the file
+ *
+ * @return {promise object}     - An object with code and an error array
  */
 function writeFile(fileName, outputPath, fileContent) {
 	const result = {
@@ -178,22 +192,18 @@ function writeFile(fileName, outputPath, fileContent) {
 
 	return new Promise((resolve, reject) => {
 		// create directory if it doesn't already exist
-		if (!fs.existsSync(outputPath)) {
-			fs.mkdirSync(outputPath, { recursive: true }, (err) => {
-				if (err) {
-					result.code = 1;
-					result.errors.push(`Error creating directory: ${err}`);
-					reject(result);
-				}
-				resolve(result);
-			});
+		const dirCreation = createDir(outputPath);
+		if (dirCreation !== 'success') {
+			result.code = 1;
+			result.errors.push(dirCreation);
+			reject(result);
 		}
 
 		// write file to specified outputPath with fileContent
-		fs.writeFileSync(`${outputPath}/${fileName}`, fileContent, (err) => {
-			if (err) {
+		fs.writeFile(`${outputPath}/${fileName}`, fileContent, (error) => {
+			if (error) {
 				result.code = 1;
-				result.errors.push(`Error outputting file: ${err}`);
+				result.errors.push(`Error outputting file: ${error}`);
 				reject(result);
 			}
 			resolve(result);
@@ -201,8 +211,33 @@ function writeFile(fileName, outputPath, fileContent) {
 	});
 }
 
+/**
+ * Create all directories within a given path
+ *
+ * @param  {string} dirPath - The path to the directory that needs creation
+ *
+ * @return {string}         - An error string or "success"
+ */
+function createDir(dirPath) {
+	D.header('createDir', { dirPath });
+
+	if (!fs.existsSync(dirPath)) {
+		D.log(`Creating directory ${color.yellow(dirPath)}`);
+
+		fs.mkdirSync(dirPath, { recursive: true }, (error) => {
+			if (error) {
+				return `Creating directory ${color.yellow(dirPath)} failed\n${error}`;
+			}
+		});
+		return 'success';
+	} else {
+		return 'success';
+	}
+}
+
 module.exports = exports = {
 	FILES,
 	saveFiles,
 	writeFile,
+	createDir,
 };
