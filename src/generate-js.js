65eee0713cb6cbb4b2cf7ff7be9ef55f1f650386
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2020-04-06 17:23:55 +1000

    added comments to code and output
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/generate-js.js
+++ src/generate-js.js
@@ -1,15 +1,22 @@
 /**
- * generateJSFile - Generates all the javascript
+ * All functions for the generator
  *
- * @param  {type} pkg - The package object with blender key
- * @return {type}     - A result object with a js key
- */
+ * generateJSFile - Generates all the javascript
+ **/
 const path = require('path');
 const fs = require('fs');
 
+const { version } = require('../package.json');
 const { color } = require('./color.js');
 const { D } = require('./log.js');
 
+/**
+ * Generates all the javascript
+ *
+ * @param  {object} pkg - The package object with blender key
+ *
+ * @return {object}     - A result object with a js key
+ */
 function generateJSFile(pkg) {
 	D.header('generateJSFile', { pkg });
 	const pkgPath = path.normalize(`${pkg.path}/${pkg.pkg.js}`);
@@ -27,6 +34,8 @@ function generateJSFile(pkg) {
 		result.message = `An error occured when trying to open ${color.yellow(pkgPath)}`;
 		D.error(`Unable to find ${pkg.pkg.js} at "${color.yellow(pkg.path)}"`);
 	}
+
+	result.js = `/*! ${pkg.name} v${pkg.version} blended with blender v${version} */\n${result.js}`;
 
 	return result;
 }
